870ea17c18e482469f42f6c7ded415be287a898e
harshit001-2023 <harshitpande938@gmail.com>
2025-10-06 22:10:17 +0530

    Solved Some errors related to databbase connection [06/10/2025]

--- VotingApp/src/main/java/com/voting/votingapp/models/Poll.java
+++ VotingApp/src/main/java/com/voting/votingapp/models/Poll.java
@@ -1,11 +1,11 @@
 package com.voting.votingapp.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,4 +16,11 @@ public class Poll {
 
     private Long id;
     private String question;
+
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection
+    private List<Long> votes = new ArrayList<>();
 }
